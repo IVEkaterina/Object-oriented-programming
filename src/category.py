34645ced25faba4e26d9ceedefb6f0a6bb78c679
Ekaterina <kkarmyjjk@gmail.com>
2025-05-06 16:57:43 +0300

    added a method add_product

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс для категории"""
     name: str
@@ -10,7 +13,11 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, products):
+        self.__products.append(products)
+        Category.product_count += 1
