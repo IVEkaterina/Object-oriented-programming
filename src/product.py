d810dd0c1cea17a1d14ca4a6d872c1d917551314
Ekaterina <kkarmyjjk@gmail.com>
2025-05-07 11:28:38 +0300

    add getter and setter for price

--- src/product.py
+++ src/product.py
@@ -8,7 +8,7 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
@@ -18,3 +18,14 @@ class Product:
         price = product["price"]
         quantity = product["quantity"]
         return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
