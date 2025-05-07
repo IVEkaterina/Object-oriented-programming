b43817124015a7f56f52640929586eb5dfd02a8b
Ekaterina <kkarmyjjk@gmail.com>
2025-05-07 11:18:47 +0300

    wrote getter products

--- src/category.py
+++ src/category.py
@@ -21,3 +21,10 @@ class Category:
     def add_product(self, products):
         self.__products.append(products)
         Category.product_count += 1
+
+    @property
+    def products(self):
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
