2abd6d4866aab07f6853469b34ed16ac12d9061c
Ekaterina <kkarmyjjk@gmail.com>
2025-05-10 06:12:49 +0300

    add class Smartphone and LawnGrass

--- src/product.py
+++ src/product.py
@@ -41,3 +41,31 @@ class Product:
             print("Цена не должна быть нулевая или отрицательная")
         else:
             self.__price = new_price
+
+class Smartphone(Product):
+    """ Класс для смартфонов """
+    efficiency: float
+    model: str
+    memory: int
+    color: str
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: float, model: str, memory: int, color: str):
+        """ Конструктор для смартфонов """
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+class LawnGrass(Product):
+    """ Класс для газонной травы"""
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: str,  color: str):
+        """ Конструктор для газонной травы """
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
