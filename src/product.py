fbbcdc95e060fd13f5f9a43602ee8d47a520656a
Ekaterina <kkarmyjjk@gmail.com>
2025-05-10 05:31:08 +0300

    add docstring and annotations for product and category

--- src/product.py
+++ src/product.py
@@ -5,20 +5,24 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        """ Конструктор для продукта """
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
     def __str__(self):
+        """ Строковое отображение класса Product """
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
+        """ Сложение всей стоимости товаров(цена*кол-во) """
         return (self.__price*self.quantity) + (other.__price*other.quantity)
 
     @classmethod
     def new_product(cls, product: dict):
+        """ Класс-метод, возвращающий созданный объект класса Product """
         name = product["name"]
         description = product["description"]
         price = product["price"]
@@ -27,10 +31,12 @@ class Product:
 
     @property
     def price(self):
+        """ Метод, возвращающий цену """
         return self.__price
 
     @price.setter
     def price(self, new_price):
+        """ Метод для проверки положительности цены"""
         if new_price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
         else:
